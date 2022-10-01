b34bfcbc79e0f97694e6b416b9c202432ec8ae04
Jacob Helfman <jacobjhelfman@gmail.com>
2022-10-01 11:13:44 -0700

    first commit_1

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Create list of existing file geodatabases
+
+# https://developers.arcgis.com/rest/users-groups-and-items/item.htm
+# Enter file path without quotes!
+
+# https://support.esri.com/en/technical-article/000018909
+
+# https://www.google.com/search?q=downloadable+agol+items&rlz=1CDGOYI_enUS949US949&oq=downloadable+agol+items+&aqs=chrome..69i57.13478j0j4&hl=en-US&sourceid=chrome-mobile&ie=UTF-8
---
+++ backup_layers.py
@@ -0,0 +1,42 @@
+from arcgis.gis import GIS
+
+import datetime as dt
+
+username = input("Input your username: ")
+
+gis = GIS('https://arcgis.com', username)
+
+folder_path = input("Please enter the file location to store the backups: ") 
+# num_items = int(input("How many items do you want to back up? "))
+query_string = "type:Feature Service, owner:{}".format(username)
+items = gis.content.search(query=query_string, sort_field='modifed', sort_order='desc')
+
+print("There are a total of " + str(len(items)) + " Feature Services that will be backed up to " + folder_path)
+
+
+def create_list(item_list):
+    fgdbs = []
+    for item in item_list:
+        if item.type == 'File Geodatabase':
+            fgdbs.append(item)
+        elif item.type == 'Feature Service':
+            version = dt.datetime.now().strftime("%d_%b_%Y")
+            fgdb = item.export(item.title + "_" + version, "File Geodatabase")
+            fgdbs.append(fgdb)
+    return fgdbs
+
+def download(fgdbs, backup_location):
+    for fgdb in fgdbs:
+        try:
+            print("Downloading " + fgdb.title)
+            fgdb.download(backup_location)
+            # result.delete()
+            print("Successfully downloaded " + fgdb.title)
+        except Exception as e:
+            print("An error occurred downloading " + fgdb.title + ": ")
+            print(e)
+    print("The download function has completed")
+
+fgdbs = create_list(items)
+print(fgdbs)
+download(fgdbs, folder_path)
